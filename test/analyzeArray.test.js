766fc768842df267a370decf767c69201f2531da
astragenius <darthmalak86@gmail.com>
2023-01-24 21:30:36 +0100

    add - test functions

--- test/analyzeArray.test.js
+++ test/analyzeArray.test.js
@@ -3,7 +3,30 @@ const analyzeArray = require('../code/analyzeArray')
 
 describe('test', () => {
 
-    test('arraytest', () => {
-        expect(analyzeArray([1, 2, 3, 4, 5])).toBe('d')
+    test('get error msg when string in array', () => {
+        expect(analyzeArray(['1', 5, 9, 20, 4])).toBe('ERROR')
     })
+
+    test('get error msg when array is emty', () => {
+        expect(analyzeArray([])).toBe('Array is emty')
+    })
+
+    test('get object from analyzer function', () => {
+        expect(analyzeArray([1, 2, 3, 4, 5])).toStrictEqual({
+            average: 3,
+            min: 1,
+            max: 5,
+            length: 5
+        })
+    })
+
+    test('get object from analyzer function', () => {
+        expect(analyzeArray([4, 12, 63, 2, 125, 88, 6, 34, 76])).toStrictEqual({
+            average: 46, // 45
+            min: 2,
+            max: 125,
+            length: 9
+        })
+    })
+
 })
